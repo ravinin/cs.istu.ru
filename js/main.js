a0a2a47b6f113b887348a39f39a6614a5ecd88f2
Vitaly Ovsyannikov <vitaly.ovsyannikov@gmail.com>
2013-06-09 17:41:59 +0400

    На 70% свёрстана главная страница, дописано ТЗ.

---
+++ js/main.js
@@ -0,0 +1,5 @@
+$(document).ready(function() {
+    $('a:not(:has(img))').addClass('link');
+/*     $(".fancybox").attr('rel', 'gallery').fancybox({padding: 0}); */
+    $(".pseudo-menu").sticky({topSpacing:0});
+});
