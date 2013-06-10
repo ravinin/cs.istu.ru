937223ecaa49c2257c914785fa5c866fe0c0d01b
Vitaly Ovsyannikov <vitaly.ovsyannikov@gmail.com>
2013-06-10 16:45:18 +0400

    Прижал футер к нижнему краю экрана, добавил страницу «Разработки»

--- js/main.js
+++ js/main.js
@@ -2,5 +2,5 @@ $(document).ready(function() {
     $('a:not(:has(img))').addClass('link');
 /*     $(".fancybox").attr('rel', 'gallery').fancybox({padding: 0}); */
     $(".pseudo-menu").sticky({topSpacing:0});
-    $('a.smooth').smoothScroll({offset: -50});
+    $('a.smooth').smoothScroll({offset: - 50});
 });
