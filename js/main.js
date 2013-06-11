83075f17f73d82cf9b23f72e794603dd3e26d504
Lev Dimov <axel12.94@gmail.com>
2013-06-11 16:48:36 +0400

    Псевдо-меню автоматически выделяется как активный при достижении конкретного якоря

--- js/main.js
+++ js/main.js
@@ -3,4 +3,29 @@ $(document).ready(function() {
 /*     $(".fancybox").attr('rel', 'gallery').fancybox({padding: 0}); */
     $(".pseudo-menu").sticky({topSpacing:0});
     $('a.smooth').smoothScroll({offset: - 50});
+
+
+    //magic
+    function getAnchorTop(element){
+        var to = element.attr("href");
+        var offset = 50;
+        return $(to).offset().top - offset;
+    }
+    var anchors = $('a.pseudo[href^="#"]');
+    function checkSectionSelected(scrolledTo){
+        var upTo = 100,
+            i;
+        for (i = 0; i < anchors.length; i++) {
+            var anchor = $(anchors[i]);
+            var target = getAnchorTop(anchor);
+            if (scrolledTo > target - upTo && scrolledTo < target + upTo) {
+                anchors.removeClass("active");
+                anchor.addClass("active", 1000);
+            }
+        }
+    }
+    checkSectionSelected($(window).scrollTop());
+    $(window).scroll(function(e){
+        checkSectionSelected($(window).scrollTop())
+    });
 });
